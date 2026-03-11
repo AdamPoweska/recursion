bc1c1c74b983541817a4b11c8993c4aa1b6cd723
Adam Poweska <adam.poweska@gmail.com>
2026-03-11 13:25:25 +0100

    better print

--- RECURSION_FIND_COMBINATIONS.py
+++ RECURSION_FIND_COMBINATIONS.py
@@ -16,4 +16,7 @@ def list_of_comb(iterable, num_to_find):
 
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print("Final combinations:", list_of_comb(numbers, 9))
+comb_num_to_be_found = 9
+print(f"Combinations from {numbers} that sum up to {comb_num_to_be_found} are {list_of_comb(numbers, comb_num_to_be_found)}")
+
+#  Combinations from [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] that sum up to 9 are [[4, 3, 2], [5, 3, 1], [5, 4], [6, 2, 1], [6, 3], [7, 2], [8, 1], [9]]
